0d86798cd1ac9a61fc1b25d04f738b4269474d2f
Adriano Veiga Botelho <adriano.botelho@lactec.org.br>
2020-08-04 05:18:19 -0300

    [cod] Aula 1 - continuando tela do TeacherList: criando card para o conteudo de cada professor na lista (mobile first)

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 import './styles.css';
 
 function TeacherList() {
@@ -25,6 +27,35 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://i.pravatar.cc/150" alt="Avatar"/>
+            <div>
+              <strong>John Doe</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br/><br/>
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 20,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
