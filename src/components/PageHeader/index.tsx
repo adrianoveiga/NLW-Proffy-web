c6210ddf7cc86c8f29980b0b31c38187ad9bb04f
Adriano Veiga Botelho <adriano.botelho@lactec.org.br>
2020-08-04 03:53:05 -0300

    [cod] Aula 1 - criando Components - criando pasta para colocar componentes e o primeiro componente, PageHeader - o componente poderá ser reutilizado em pages diferentes e pode receber parametros - criado componente, mostrando a sintaxe a ser usada para poder ler os parametros direto do JSX, com o React.FC<> - explicado a utilização de interfaces para definir os parâmetros que o componente deve receber, tanto opcional quanto obrigatório

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+// Precisa criar uma interface que irá definir os parâmetros do componente
+// Syntax:
+//   param: type;    (obrigatório)
+//   param?: type;   (opcional)
+interface PageHeaderProps {
+  title: string;
+}
+
+
+// A função deve se tornar algo constante e usar a notação de arrow function
+// Deve usar o React.FC (ou React.FunctionComponent)
+// Os parâmetros "props" conterá tudo o que for passado ao componente definido na interface
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
+  return (
+    <header className="page-header">
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar"/>
+        </Link>
+        <img src={logoImg} alt="Proffy"/>
+      </div>
+
+      <div className="header-content">
+        <strong>{props.title}</strong>
+      </div>
+    </header>
+  );
+}
+
+export default PageHeader;
