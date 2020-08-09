d471941172f3c9a6e878b5bad3983c81577568e2
Adriano Veiga Botelho <adriano.botelho@lactec.org.br>
2020-08-09 03:10:25 -0300

    [cod] Aula 3 - conectando a tela de lista de professores com a busca no backend: - utilizando os mesmos conceitos vistos anteriormente com useState para relacionar informações na interface e atualizar com base no que vier do backend; - acrescentado botão na interface de busca, para apenas buscar ao apertar o botão; - passando um objeto inteiro (através de interfaces) como propriedade do TeacherItem;

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,27 +4,38 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher } ) => {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://i.pravatar.cc/150" alt="Avatar"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>John Doe</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br/><br/>
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 20,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="Whatsapp"/>
