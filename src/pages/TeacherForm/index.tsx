3a249ce2e2591cbea42872639d507e270f2bf8b9
Adriano Veiga Botelho <adriano.botelho@lactec.org.br>
2020-08-09 01:58:37 -0300

    [cod] Aula 3 - utilizando State para pegar o estado em variáveis javascript de cada input simples do formulário e passando para uma função que irá tratar ao receber o submit do form

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
@@ -11,6 +11,14 @@ import './styles.css';
 
 function TeacherForm() {
 
+  const [name, setName] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [bio, setBio] = useState('');
+
+  const [subject, setSubject] = useState('');
+  const [cost, setCost] = useState('');
+
   const [scheduleItems, setScheduleItems] = useState([
     { week_day: 0, from: '', to: '' }
   ]);
@@ -22,6 +30,19 @@ function TeacherForm() {
     ]);
   }
 
+  function handleCreateClass(ev: FormEvent) {
+    ev.preventDefault();
+
+    console.log({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost
+    });
+  }
+
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader
@@ -30,82 +51,111 @@ function TeacherForm() {
       />
 
       <main>
-        <fieldset>
-          <legend>Seus dados</legend>
+        <form onSubmit={handleCreateClass}>
+          <fieldset>
+            <legend>Seus dados</legend>
 
-          <Input name="name" label="Nome Completo"/>
-          <Input name="avatar" label="Avatar"/>
-          <Input name="whatsapp" label="Whatsapp"/>
-          <TextArea name="bio" label="Biografia" />
-        </fieldset>
+            <Input
+              name="name"
+              label="Nome Completo"
+              value={name}
+              onChange={(ev) => { setName(ev.target.value) }}
+            />
+            <Input
+              name="avatar"
+              label="Avatar"
+              value={avatar}
+              onChange={(ev) => { setAvatar(ev.target.value) }}
+            />
+            <Input
+              name="whatsapp"
+              label="Whatsapp"
+              value={whatsapp}
+              onChange={(ev) => { setWhatsapp(ev.target.value) }}
+            />
+            <TextArea
+              name="bio"
+              label="Biografia"
+              value={bio}
+              onChange={(ev) => { setBio(ev.target.value) }}
+            />
+          </fieldset>
 
-        <fieldset>
-          <legend>Sobre a aula</legend>
+          <fieldset>
+            <legend>Sobre a aula</legend>
 
-          <Select
-            name="subject"
-            label="Matéria"
-            options={[
-              { value: "Artes", label: "Artes" },
-              { value: "Biologia", label: "Biologia" },
-              { value: "Ciências", label: "Ciências" },
-              { value: "Educação Física", label: "Educação Física" },
-              { value: "Física", label: "Física" },
-              { value: "Geografia", label: "Geografia" },
-              { value: "História", label: "História" },
-              { value: "Matemática", label: "Matemática" },
-              { value: "Português", label: "Português" },
-              { value: "Química", label: "Química" },
-              { value: "Redação", label: "Redação" },
-            ]}
-          />
-          <Input name="cost" label="Custo da sua aula por hora"/>
-        </fieldset>
+            <Select
+              name="subject"
+              label="Matéria"
+              value={subject}
+              onChange={(ev) => { setSubject(ev.target.value) }}
+              options={[
+                { value: "Artes", label: "Artes" },
+                { value: "Biologia", label: "Biologia" },
+                { value: "Ciências", label: "Ciências" },
+                { value: "Educação Física", label: "Educação Física" },
+                { value: "Física", label: "Física" },
+                { value: "Geografia", label: "Geografia" },
+                { value: "História", label: "História" },
+                { value: "Matemática", label: "Matemática" },
+                { value: "Português", label: "Português" },
+                { value: "Química", label: "Química" },
+                { value: "Redação", label: "Redação" },
+              ]}
+            />
+            <Input
+              name="cost"
+              label="Custo da sua aula por hora"
+              value={cost}
+              onChange={(ev) => { setCost(ev.target.value) }}
+            />
+          </fieldset>
 
-        <fieldset>
-          <legend>
-            {/* NOTA: Essa div só existe para compatibilidade do flex entre Firefox e Chrome (sem ela não funciona corretamente no FF) */}
-            <div>
-              Horários disponíveis
-              <button type="button" onClick={addNewScheduleItem}>
-                + Novo horário
-              </button>
-            </div>
-          </legend>
-
-          {scheduleItems.map(scheduleItem => {
-            return (
-              <div key={scheduleItem.week_day} className="schedule-item">
-                <Select
-                  name="week_day"
-                  label="Dia da semana"
-                  options={[
-                    { value: "0", label: "Domingo" },
-                    { value: "1", label: "Segunda-feira" },
-                    { value: "2", label: "Terça-feira" },
-                    { value: "3", label: "Quarta-feira" },
-                    { value: "4", label: "Quinta-feira" },
-                    { value: "5", label: "Sexta-feira" },
-                    { value: "6", label: "Sábado" },
-                  ]}
-                />
-                <Input name="from" label="Das" type="time" />
-                <Input name="to" label="Até" type="time" />
+          <fieldset>
+            <legend>
+              {/* NOTA: Essa div só existe para compatibilidade do flex entre Firefox e Chrome (sem ela não funciona corretamente no FF) */}
+              <div>
+                Horários disponíveis
+                <button type="button" onClick={addNewScheduleItem}>
+                  + Novo horário
+                </button>
               </div>
-            )
-          })}
-        </fieldset>
+            </legend>
 
-        <footer>
-          <p>
-            <img src={warningIcon} alt="Aviso importante"/>
-            Importante! <br />
-            Preencha todos os dados
-          </p>
-          <button type="button">
-            Salvar Cadastro
-          </button>
-        </footer>
+            {scheduleItems.map(scheduleItem => {
+              return (
+                <div key={scheduleItem.week_day} className="schedule-item">
+                  <Select
+                    name="week_day"
+                    label="Dia da semana"
+                    options={[
+                      { value: "0", label: "Domingo" },
+                      { value: "1", label: "Segunda-feira" },
+                      { value: "2", label: "Terça-feira" },
+                      { value: "3", label: "Quarta-feira" },
+                      { value: "4", label: "Quinta-feira" },
+                      { value: "5", label: "Sexta-feira" },
+                      { value: "6", label: "Sábado" },
+                    ]}
+                  />
+                  <Input name="from" label="Das" type="time" />
+                  <Input name="to" label="Até" type="time" />
+                </div>
+              )
+            })}
+          </fieldset>
+
+          <footer>
+            <p>
+              <img src={warningIcon} alt="Aviso importante"/>
+              Importante! <br />
+              Preencha todos os dados
+            </p>
+            <button type="submit">
+              Salvar Cadastro
+            </button>
+          </footer>
+        </form>
       </main>
     </div>
   );
